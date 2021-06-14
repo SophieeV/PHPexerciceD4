b55b101fc554c975700d99292a1c4de41ca9eef3
SophieeV <sophievesin@wanadoo.fr>
2021-06-14 12:05:27 +0200

    ajout fichier

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice D2</title>
+</head>
+<body>
+
+<?php 
+
+$languages = array (
+    1 => 'html',
+    2 => 'css',
+    3 => 'javascript',
+    4 => 'PHP',
+    5 => 'SQL',
+);
+array_push($languages, 'python');
+print_r($languages[6]);
+ 
+
+?>
+    
+</body>
+</html>
